b3c93508ba497bae2c903f43cd4cc29678b47119
Nitzan Moyal <peretz.nitzan@gmail.com>
2018-10-09 20:39:01 +0300

    DB_URL

--- server/db.js
+++ server/db.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 
-const DB_URL= 'mongodb://admin:qwer1234@ds123603.mlab.com:23603/pinterest-db'
-//const DB_URL = process.env.DB_URL || 'mongodb://localhost:27017/pinterest-DB'
-//let DB_URL = 'mongodb://localhost:27017/pinterest-DB';
+const DB_URL = process.env.DB_URL || 'mongodb://localhost:27017/pinterest-DB'
 mongoose.connect(DB_URL);
 
 
